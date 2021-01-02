2d87ea918ebd016c1d50462c1d6fb5b09f70997f
NABE Bammoy Minkitam <nabe.minkitam@gmail.com>
2021-01-02 02:24:46 +0000

    Remove unnecessary router configuration because of CORS middleware

--- src/routers/v1/api_router.js
+++ src/routers/v1/api_router.js
@@ -3,16 +3,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-  next();
-});
-
 // router.use('/user', userRouter);
 
 module.exports = router;
--- src/routers/v1/auth_router.js
+++ src/routers/v1/auth_router.js
@@ -3,16 +3,6 @@ const authRouter = require('../../modules/users/auth_router');
 
 const router = express.Router();
 
-router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-  next();
-});
-
 router.use('/auth', authRouter);
 
 module.exports = router;
